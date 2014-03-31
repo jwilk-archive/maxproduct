66983ac8858b79d5a4a6448b77d28c7af4839844
Jakub Wilk <jwilk@jwilk.net>
2014-03-31 13:43:07 +0200

    Add a proper command-line interface.

--- lib.py
+++ lib.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import operator
 from heapq import heappush, heappushpop
 
@@ -14,9 +16,5 @@ def maxproduct(xs, ys, *, n, fn=operator.mul):
                 else:
                     break
     return sorted(t)
-
-if __name__ == '__main__':
-    from pprint import pprint
-    pprint(maxproduct(range(1000), range(1000), n=10000))
 
 # vim:ts=4 sw=4 et
